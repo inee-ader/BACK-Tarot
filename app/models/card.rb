41c892ea257002200c8850191958ba61c1c0a379
xoliio <xoliio@gmail.com>
2021-03-17 16:34:23 -0700

    adds validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,7 @@
 class Card < ApplicationRecord
-    
+    validates :name, :flower, :meaning, :reverse, :reflection, :commentary, :image_url, presence: true
+
+    validates :name, :flower, :meaning, :reverse, :reflection, :commentary, :image_url, uniqueness: true
+
+    validates :name, :flower, :meaning, :reverse, :reflection, format: { with: /\A[a-zA-Z]+\z/, message: "letters only" }
 end
